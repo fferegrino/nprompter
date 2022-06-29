da821dff4f9d338988aa5214e26c2b31ea8cdc8e
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-06-29 06:14:07 +0100

    More js commands

--- nprompter/web/assets/script.js
+++ nprompter/web/assets/script.js
@@ -1,15 +1,23 @@
+const content = document.getElementById('content')
+const elem = document.documentElement;
+const manualScrollAmount = 10;
+const fontSizeIncrease = 2;
+const paddingSizeIncrease = 10;
+const maxScrollSpeed = 200;
+const scrollSpeedIncrease = 3;
+const maxPadding = 250;
+const maxFontSize = 200;
+let fontSize = parseInt(getComputedStyle(content).fontSize);
+let paddingSize = parseInt(getComputedStyle(content).paddingLeft);
 let scrollTimer = 0;
 let isScrolling = false;
 let scrollSpeed = 10;
 
-const content = document.getElementById('content')
-const elem = document.documentElement;
 
 document.addEventListener('keydown', logKey);
 
 function logKey(e) {
     const keyCode = e.keyCode;
-    console.log(keyCode)
     if (handleKeyCode(keyCode)) {
         e.preventDefault();
     }
@@ -28,10 +36,35 @@ function openFullscreen() {
     }
 }
 
+
 const controls = {
     27: [function() {
         window.scrollTo(0, 0)
     }, "Scroll to top", 'esc'],
+    37: [function() {
+        scrollSpeed = Math.min(maxScrollSpeed, scrollSpeed + scrollSpeedIncrease)
+    }, "Decrease speed", '→'],
+    39: [function() {
+        scrollSpeed = Math.max(0, scrollSpeed - scrollSpeedIncrease)
+    }, "Increase speed", '←'],
+    80: [function() {
+        paddingSize = Math.min(maxPadding, paddingSize + paddingSizeIncrease);
+        content.style.paddingLeft = paddingSize + "px"
+        content.style.paddingRight = paddingSize + "px"
+    }, "Increase padding", 'P'],
+    79: [function() {
+        paddingSize = Math.max(0, paddingSize - paddingSizeIncrease);
+        content.style.paddingLeft = paddingSize + "px"
+        content.style.paddingRight = paddingSize + "px"
+    }, "Decrease padding", 'O'],
+    85: [function() {
+        fontSize = Math.min(maxFontSize, fontSize + fontSizeIncrease);
+        content.style.fontSize = fontSize + "px"
+    }, "Increase font size", 'U'],
+    68: [function() {
+        fontSize = Math.max(0, fontSize - fontSizeIncrease);
+        content.style.fontSize = fontSize + "px"
+    }, "Decrease font size", 'D'],
     32: [toggleScrolling, "Start scroll", 'space'],
     70: [openFullscreen, "Fullscreen", 'f'],
     77: [function() {
@@ -44,12 +77,10 @@ function handleKeyCode(keyCode) {
     if (keyCode in controls) {
         const [action, docs, key] = controls[keyCode]
         action()
+        console.log(docs)
     } else {
         handled = false;
         console.log("Not handled " + keyCode)
-    }
-    if (handled) {
-        // updateDebug();
     }
     return handled;
 
