04754eb6ca6d8f153874fc62952f7796b64cfce7
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-10-07 07:21:47 +0100

    docs: Add documentation for commands

--- nprompter/__main__.py
+++ nprompter/__main__.py
@@ -4,49 +4,70 @@ import socketserver
 import webbrowser
 from http.server import SimpleHTTPRequestHandler
 from pathlib import Path
-from typing import Any, Optional, Union
+from typing import Any, Optional
 
 import typer
 
 import nprompter
 import nprompter.web
 from nprompter.api.notion_client import NotionClient
+from nprompter.cli.defaults import DEFAULT_OUTPUT_PATH, DEFAULT_PORT, DEFAULT_PROPERTY, DEFAULT_VALUE
 from nprompter.cli.helpers import get_config
 from nprompter.processing.processor import HtmlNotionProcessor
 
 app = typer.Typer(add_completion=False)
 
-DEFAULT_PATH = ".content"
-
 
 @app.command()
 def build(
-    database_id: str,
-    content_directory: Union[str, None] = DEFAULT_PATH,
-    property_filter: Optional[str] = "Status",
-    property_value: Optional[str] = "Ready",
-    config: Optional[Path] = typer.Option("config.toml"),
-    custom_css: Optional[Path] = typer.Option(None),
-    just_assets: bool = False,
+    database_id: str = typer.Argument(..., help="The Notion's database to process"),
+    notion_api_key: str = typer.Argument(None, envvar="NOTION_API_KEY", help="The Notion API key to use"),
+    output: Optional[Path] = typer.Option(
+        DEFAULT_OUTPUT_PATH, "--output", "-o", help="Where the output should be written"
+    ),
+    property_filter: Optional[str] = typer.Option(
+        DEFAULT_PROPERTY, "--filter", "-f", help="The name of the Notion's page property to filter by"
+    ),
+    property_value: Optional[str] = typer.Option(
+        DEFAULT_VALUE, "--value", "-v", help="The value of the Notion's page property to filter by"
+    ),
+    appearance_file: Optional[Path] = typer.Option(
+        None, "--config", "-c", help="A path to an appearance configuration file"
+    ),
+    custom_css: Optional[Path] = typer.Option(None, "--extra-css", "-s", help="A path to extra css configuration"),
+    download_database: bool = typer.Option(
+        True, "--download/--no-download", help="Whether to sync with the Notion's database"
+    ),
 ):
-    notion_api_key = os.environ["NOTION_API_KEY"]
+    """
+    Fetch a Notion database and create the teleprompter's pages from it
+    """
     notion_version = os.getenv("NOTION_VERSION", nprompter.__notion_version__)
 
     notion_client = NotionClient(notion_api_key=notion_api_key, notion_version=notion_version)
-    processor = HtmlNotionProcessor(notion_client, output_folder=content_directory)
+    processor = HtmlNotionProcessor(notion_client, output_folder=output)
 
-    config_dict = get_config(config)
+    config_dict = get_config(appearance_file)
     processor.prepare_folder(config_dict)
 
     if custom_css:
         processor.add_extra_style(custom_css)
 
-    if not just_assets:
+    if download_database:
         processor.process_databases(database_id, property_filter, property_value)
 
 
 @app.command()
-def serve(port: int = 8889, content_directory: Union[str, None] = DEFAULT_PATH):
+def serve(
+    port: int = typer.Argument(DEFAULT_PORT),
+    content_directory: Path = typer.Argument(DEFAULT_OUTPUT_PATH),
+    browser: bool = typer.Option(True, help="Whether to open a web browser pointing to the server's address"),
+):
+    """
+    Start a basic web server to display the teleprompter's pages.
+    This a good option for a small scale deployment.
+    """
+
     class CustomHandler(SimpleHTTPRequestHandler):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, directory=content_directory, **kwargs)
@@ -58,7 +79,8 @@ def serve(port: int = 8889, content_directory: Union[str, None] = DEFAULT_PATH):
     with socketserver.TCPServer(("", port), CustomHandler) as httpd:
         location = f"http://localhost:{port}"
         print(f"Serving at {location}")
-        webbrowser.open(location)
+        if browser:
+            webbrowser.open(location)
         httpd.serve_forever()
 
 
