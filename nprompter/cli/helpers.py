089e212f53f578f6ca130d0085a2fd74db25b53e
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-02-18 21:49:33 +0000

    fix: Changed implementation of dict update for the config file

--- nprompter/cli/helpers.py
+++ nprompter/cli/helpers.py
@@ -1,17 +1,29 @@
 import importlib
+import importlib.resources
 import os
 
 import tomli
 
 import nprompter
 import nprompter.cli.defaults
+import nprompter.web
+
+
+def recursive_update(base, new):
+    # Recursively update a dictionary with another dictionary
+    for k, v in new.items():
+        if isinstance(v, dict):
+            base[k] = recursive_update(base.get(k, {}), v)
+        else:
+            base[k] = v
+    return base
 
 
 def get_config(configuration_file):
-    config_dict = tomli.loads(importlib.resources.read_text(nprompter.web, nprompter.cli.defaults.DEFAULT_CONFIG_PATH))
+    base_config = tomli.loads(importlib.resources.read_text(nprompter.web, nprompter.cli.defaults.DEFAULT_CONFIG_PATH))
     configuration_file = configuration_file or nprompter.cli.defaults.DEFAULT_CONFIG_PATH
     if os.path.exists(configuration_file):
         with open(configuration_file, "rb") as readable:
             user_defined_config = tomli.load(readable)
-            config_dict.update(user_defined_config)
-    return config_dict
+        base_config = recursive_update(base_config, user_defined_config)
+    return base_config
