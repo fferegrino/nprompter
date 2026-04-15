10ffd20faebd34226a9c910c4d7e9d1670402e58
Antonio Feregrino <antonio.feregrino@gmail.com>
2026-04-15 21:34:34 +0100

    chore: Migrate version management

--- release.sh
+++ release.sh
@@ -3,10 +3,10 @@
 TOOL_RUN="uv run"
 
 function bump {
-  NEW_VERSION=`${TOOL_RUN} bumpversion --allow-dirty --dry-run --list $1 | grep new_version | sed -r s,"^.*=",,`
+  NEW_VERSION="$(${TOOL_RUN} bump-my-version show --increment "$1" new_version)"
   ${TOOL_RUN} cz changelog --file-name changelog.md --unreleased-version="v${NEW_VERSION}"
   git add changelog.md && git commit -m "Update changelog.md for v${NEW_VERSION}"
-  ${TOOL_RUN} bumpversion $1
+  ${TOOL_RUN} bump-my-version bump "$1" --allow-dirty
 }
 
 
